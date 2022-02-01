e3d9d9475f087e04d5f242a2a413b6e11597889c
Sarvarian <Sarvarian@Hotmail.com>
2022-02-01 17:57:12 +0330

    Update main.h

--- src/os/include/os/main.h
+++ src/os/include/os/main.h
@@ -8,36 +8,36 @@ int __aban_os_init_main(int argc, char *argv[]);
 int __aban_os_failed_init(int error_code);
 int __aban_os_exit();
 
-#define __MainBody(proc, init, ...)         \
-  const int init_res = init(##__VA_ARGS__); \
-  if (init_res != ABAN_INIT_SUCCESS)        \
-  {                                         \
-    return __aban_os_failed_init(init_res); \
-  }                                         \
-  else                                      \
-  {                                         \
-    proc();                                 \
-    __aban_os_exit();                       \
-    return 0;                               \
-  }
+#define __MainBody(proc, init, ...)       \
+ const int init_res = init(__VA_ARGS__);  \
+ if (init_res != ABAN_INIT_SUCCESS)       \
+ {                                        \
+  return __aban_os_failed_init(init_res); \
+ }                                        \
+ else                                     \
+ {                                        \
+  proc();                                 \
+  __aban_os_exit();                       \
+  return 0;                               \
+ }
 
 #ifdef _WIN32
-#define Main(proc)                                                                                \
-  int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) \
-  {                                                                                               \
-    __MainBody(proc, __aban_os_init_winmain, hInstance, pCmdLine, nCmdShow)                       \
-  }                                                                                               \
-                                                                                                  \
-  int main(int argc, char *argv[])                                                                \
-  {                                                                                               \
-    __MainBody(proc, __aban_os_init_main, argc, argv)                                             \
-  }
+#define Main(proc)                                                                               \
+ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) \
+ {                                                                                               \
+  __MainBody(proc, __aban_os_init_winmain, hInstance, pCmdLine, nCmdShow)                        \
+ }                                                                                               \
+                                                                                                 \
+ int main(int argc, char *argv[])                                                                \
+ {                                                                                               \
+  __MainBody(proc, __aban_os_init_main, argc, argv)                                              \
+ }
 
 #else
-#define Main(proc)                                    \
-  int main(int argc, char *argv[])                    \
-  {                                                   \
-    __MainBody(proc, __aban_os_init_main, argc, argv) \
-  }
+#define Main(proc)                                  \
+ int main(int argc, char *argv[])                   \
+ {                                                  \
+  __MainBody(proc, __aban_os_init_main, argc, argv) \
+ }
 
 #endif // _WIN32
