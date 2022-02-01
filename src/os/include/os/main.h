8620574268550dfdf1499154aaaee336f7124d49
Sarvarian <Sarvarian@Hotmail.com>
2022-02-01 16:37:47 +0330

    Update main.h

--- src/os/include/os/main.h
+++ src/os/include/os/main.h
@@ -8,32 +8,36 @@ int __aban_os_init_main(int argc, char *argv[]);
 int __aban_os_failed_init(int error_code);
 int __aban_os_exit();
 
+#define __MainBody(proc, init, ...)         \
+  const int init_res = init(##__VA_ARGS__); \
+  if (init_res != ABAN_INIT_SUCCESS)        \
+  {                                         \
+    return __aban_os_failed_init(init_res); \
+  }                                         \
+  else                                      \
+  {                                         \
+    proc();                                 \
+    __aban_os_exit();                       \
+    return 0;                               \
+  }
+
+#ifdef _WIN32
 #define Main(proc)                                                                                \
   int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) \
   {                                                                                               \
-    const int init_res = __aban_os_init_winmain(hInstance, pCmdLine, nCmdShow);                   \
-    if (init_res != ABAN_INIT_SUCCESS)                                                            \
-    {                                                                                             \
-      return __aban_os_failed_init(init_res);                                                     \
-    }                                                                                             \
-    else                                                                                          \
-    {                                                                                             \
-      proc();                                                                                     \
-      __aban_os_exit();                                                                           \
-      return 0;                                                                                   \
-    }                                                                                             \
+    __MainBody(proc, __aban_os_init_winmain, hInstance, pCmdLine, nCmdShow)                       \
   }                                                                                               \
                                                                                                   \
   int main(int argc, char *argv[])                                                                \
   {                                                                                               \
-    const int init_res = __aban_os_init_main(argc, argv);                                         \
-    if (init_res != ABAN_INIT_SUCCESS)                                                            \
-    {                                                                                             \
-      return __aban_os_failed_init(init_res);                                                     \
-    }                                                                                             \
-    else                                                                                          \
-    {                                                                                             \
-      proc();                                                                                     \
-      return __aban_os_exit();                                                                    \
-    }                                                                                             \
+    __MainBody(proc, __aban_os_init_main, argc, argv)                                             \
   }
+
+#else
+#define Main(proc)                                    \
+  int main(int argc, char *argv[])                    \
+  {                                                   \
+    __MainBody(proc, __aban_os_init_main, argc, argv) \
+  }
+
+#endif // _WIN32
