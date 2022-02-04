ba7c64bc3f2bfb1387b6356067e596ededeabc64
Sarvarian <Sarvarian@Hotmail.com>
2022-02-04 06:53:04 +0330

    remove dead code and refactor loop_delay initialization.

--- src/os/source/delayed_loop.c
+++ src/os/source/delayed_loop.c
@@ -6,10 +6,9 @@
 LoopDelay aban_os_loop_delay_create(u32 run_per_second)
 {
  const u32 frequency = MILISECONDS_IN_SECOND / run_per_second;
- // const LoopDelay loop_delay = {SDL_GetTicks(), frequency};
- LoopDelay loop_delay;
- loop_delay.frequency = frequency;
- loop_delay.ticks = SDL_GetTicks();
+ const LoopDelay loop_delay = {
+     .frequency = frequency,
+     .ticks = SDL_GetTicks()};
  return loop_delay;
 }
 
