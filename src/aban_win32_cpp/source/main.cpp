22ef8a2c4fa040ad6a78c51f41a60c4999c097c9
Sarvarian <Sarvarian@Hotmail.com>
2022-02-04 06:58:45 +0330

    update

--- src/aban_win32_cpp/source/main.cpp
+++ src/aban_win32_cpp/source/main.cpp
@@ -1,13 +1,21 @@
+#include <stdio.h>
 #include "os/main.h"
 #include "os/os.h"
 #include "os/window.h"
 #include "os/delayed_loop.h"
 
 inline void thread_main();
+inline void loop();
 
-Main(DelayedLoop(true, thread_main(), 30));
+Main(thread_main());
 
 inline void thread_main()
 {
+ DelayedLoop(true, loop(), 1);
+}
+
+inline void loop()
+{
  aban_os_process();
+ printf("%i\n", SDL_GetTicks());
 }
